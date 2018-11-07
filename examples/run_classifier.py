12c89608a6c98da05a84f0bd86c82e0641d10e23
Mathis Chenuet <mathis.chenuet@unchartech.com>
2018-11-07 17:06:18 +0100

    imdb processor

--- examples/run_classifier.py
+++ examples/run_classifier.py
@@ -192,6 +192,37 @@ class ColaProcessor(DataProcessor):
         return examples
 
 
+class ImdbProcessor(DataProcessor):
+    """Processor for the IMDB data set."""
+
+    def get_train_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
+
+    def get_dev_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
+
+    def get_labels(self):
+        """See base class."""
+        return ["0", "1"]
+
+    def _create_examples(self, lines, set_type):
+        """Creates examples for the training and dev sets."""
+        examples = []
+        for (i, line) in enumerate(lines):
+            if i == 0:  # skip header
+                continue
+            guid = "%s-%s" % (set_type, i)
+            text_a = tokenization.convert_to_unicode(line[1])
+            label = tokenization.convert_to_unicode(line[0])
+            examples.append(
+                InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
+        return examples
+
+
 def convert_examples_to_features(examples, label_list, max_seq_length, tokenizer):
     """Loads a data file into a list of `InputBatch`s."""
 
@@ -430,6 +461,7 @@ def main():
         "cola": ColaProcessor,
         "mnli": MnliProcessor,
         "mrpc": MrpcProcessor,
+        "imdb": ImdbProcessor,
     }
 
     if args.local_rank == -1 or args.no_cuda:
